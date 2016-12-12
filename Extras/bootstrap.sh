cdec55dc3bf3ab9726db676e6dccf885c7a48eb3
lucio.asnaghi <lucio.asnaghi@king.com>
2016-12-12 22:10:23 +0100

    - updated to use llvm + clang version 3.9

--- Extras/bootstrap.sh
+++ Extras/bootstrap.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+VERSION="release_39"
+
 if [ ! -d "llvm" ]; then
 	# install llvm
-	svn co http://llvm.org/svn/llvm-project/llvm/trunk llvm
+	svn co http://llvm.org/svn/llvm-project/llvm/branches/${VERSION} llvm
 
 	# install clang
 	pushd llvm/tools
-	svn co http://llvm.org/svn/llvm-project/cfe/trunk clang
+	svn co http://llvm.org/svn/llvm-project/cfe/branches/${VERSION} clang
 	popd
 
 	# install compiler-rt and libcxx
 	pushd llvm/projects
-	svn co http://llvm.org/svn/llvm-project/compiler-rt/trunk compiler-rt
-	svn co http://llvm.org/svn/llvm-project/libcxx/trunk libcxx
-	svn co http://llvm.org/svn/llvm-project/libcxxabi/trunk libcxxabi
+	svn co http://llvm.org/svn/llvm-project/compiler-rt/branches/${VERSION} compiler-rt
+	svn co http://llvm.org/svn/llvm-project/libcxx/branches/${VERSION} libcxx
+	svn co http://llvm.org/svn/llvm-project/libcxxabi/branches/${VERSION} libcxxabi
 	popd
 fi
